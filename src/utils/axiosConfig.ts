40b2e7d757b854c666b772d2647eaab5814d1d24
abdullahyusha <abdullahyusha999@gmail.com>
2026-06-14 19:20:04 +0600

    Fix axios bug.

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
+import https from "https";
 
-axiosRetry(axios, {
+// dsebd.org sends an incomplete certificate chain, so we skip TLS verification for it
+const insecureAgent = new https.Agent({ rejectUnauthorized: false });
+
+const instance = axios.create({
+  httpsAgent: insecureAgent,
+});
+
+axiosRetry(instance, {
   retries: 3,
   retryDelay: axiosRetry.exponentialDelay,
 });
 
-export default axios;
+export default instance;
