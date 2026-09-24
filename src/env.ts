b15cb847d86f15bb6e3bc95aec4e287ecd2a3a54
yunarch-sh <abdullahyusha999@gmail.com>
2026-09-24 10:51:00 +0600

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 const url = process.env.DSE_BASE_URL;
 
 export const DHAKA_STOCK_URLS = {
-  LATEST_DATA: `${url}/latest-share-price`,
+  LATEST_DATA: `${url}/markets/latest-share-price`,
   TOP_30: `${url}/dse30_share.php`,
   DSEX: `${url}/dseX_share.php`,
   HISTORIACAL_DATA: `${url}/day_end_archive.php`,
